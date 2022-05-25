c6c74112b43b5459f050f8ba69917dcaceb2da70
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-05-25 10:27:02 +0300

    fix(index): update render method

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import RecoilOutside from 'recoil-outside';
 import App from './App';
 import './global.css';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<RecoilRoot>
-			<RecoilOutside />
-			<App />
-		</RecoilRoot>
-	</React.StrictMode>,
-	document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+	<RecoilRoot>
+		<RecoilOutside />
+		<App />
+	</RecoilRoot>
 );
