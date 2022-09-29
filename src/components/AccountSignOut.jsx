1534642ced6b91b014824046f2daab0671889ff4
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-09-29 06:28:00 +0000

    remove cache

--- src/components/AccountSignOut.jsx
+++ src/components/AccountSignOut.jsx
@@ -1,19 +1,33 @@
+import { useState } from 'react'
+import { deepPurple } from '@mui/material/colors';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import { deepPurple } from '@mui/material/colors';
+import IconButton from '@mui/material/IconButton';
 
 const AccountSignOut = () => {
+	const [anchorEl, setAnchorEl] = React.useState(null);
+  	const open = Boolean(anchorEl);
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
 	return (
 		<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-			<Avatar
-				sx={{
-					bgcolor: deepPurple[500],
-					cursor: 'pointer',
-					'&:hover': { boxShadow: 'inset 0 0 0 2px #D7DBDD' },
-				}}
-			>
-				S
-			</Avatar>
+			<IconButton
+            	onClick={handleClick}
+            	size="small"
+            	sx={{ ml: 2 }}
+            	aria-controls={open ? 'account-menu' : undefined}
+            	aria-haspopup="true"
+            	aria-expanded={open ? 'true' : undefined}
+          	>
+            <Avatar sx={{ width: 32, height: 32, bgcolor: deepPurple[500], }}>S</Avatar>
+          </IconButton>
 		</Box>
 	);
 };
