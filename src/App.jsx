34e17fc33587a4920f292fbc05708b3f9a07e60c
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-06-20 10:19:41 +0300

    chore(web): remove unused variables

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ClientJS } from 'clientjs';
 import CssBaseline from '@mui/material/CssBaseline';
-import { apiHostState, isAdminState, isLightThemeState, isOnlineState, visitorIDState } from './states/main';
+import { apiHostState, isAdminState, isLightThemeState, visitorIDState } from './states/main';
 import { InternetChecker } from './features/internetChecker';
 import { appSnackOpenState } from './states/notification';
 import DashboardMenu from './pages/DashboardMenu';
@@ -23,117 +23,116 @@ const Users = lazy(() => import('./pages/Users'));
 
 // creating theme
 const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
+	palette: {
+		mode: 'light',
+	},
 });
 
 const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
+	palette: {
+		mode: 'dark',
+	},
 });
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  const setVisitorID = useSetRecoilState(visitorIDState);
-  const setApiHost = useSetRecoilState(apiHostState);
+	const setVisitorID = useSetRecoilState(visitorIDState);
+	const setApiHost = useSetRecoilState(apiHostState);
 
-  const isOnline = useRecoilValue(isOnlineState);
-  const isLight = useRecoilValue(isLightThemeState);
-  const appSnackOpen = useRecoilValue(appSnackOpenState);
-  const isAdmin = useRecoilValue(isAdminState);
+	const isLight = useRecoilValue(isLightThemeState);
+	const appSnackOpen = useRecoilValue(appSnackOpenState);
+	const isAdmin = useRecoilValue(isAdminState);
 
-  const [activeTheme, setActiveTheme] = useState(darkTheme);
+	const [activeTheme, setActiveTheme] = useState(darkTheme);
 
-  const router = createBrowserRouter([
-    {
-      element: <Layout />,
-      errorElement: <ErrorBoundary />,
-      children: [
-        { path: '/signin', element: <Startup /> },
-        {
-          element: <PrivateRoute isAdmin={isAdmin} />,
-          children: [
-            { path: '/', element: <DashboardMenu /> },
-            {
-              path: '/congregations',
-              element: <Congregations />,
-            },
-            {
-              path: '/congregations/:id',
-              element: <CongregationDetails />,
-            },
-            {
-              path: '/users',
-              element: <Users />,
-            },
-            {
-              path: '/users/:id',
-              element: <UserDetails />,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
+	const router = createBrowserRouter([
+		{
+			element: <Layout />,
+			errorElement: <ErrorBoundary />,
+			children: [
+				{ path: '/signin', element: <Startup /> },
+				{
+					element: <PrivateRoute isAdmin={isAdmin} />,
+					children: [
+						{ path: '/', element: <DashboardMenu /> },
+						{
+							path: '/congregations',
+							element: <Congregations />,
+						},
+						{
+							path: '/congregations/:id',
+							element: <CongregationDetails />,
+						},
+						{
+							path: '/users',
+							element: <Users />,
+						},
+						{
+							path: '/users/:id',
+							element: <UserDetails />,
+						},
+					],
+				},
+			],
+		},
+	]);
 
-  useEffect(() => {
-    if (isLight) {
-      setActiveTheme(lightTheme);
-    } else {
-      setActiveTheme(darkTheme);
-    }
-  }, [isLight]);
+	useEffect(() => {
+		if (isLight) {
+			setActiveTheme(lightTheme);
+		} else {
+			setActiveTheme(darkTheme);
+		}
+	}, [isLight]);
 
-  useEffect(() => {
-    // get visitor ID and check if there is an active connection
-    const getUserID = () => {
-      const client = new ClientJS();
-      const visitorId = client.getFingerprint();
-      setVisitorID(visitorId);
-    };
+	useEffect(() => {
+		// get visitor ID and check if there is an active connection
+		const getUserID = () => {
+			const client = new ClientJS();
+			const visitorId = client.getFingerprint();
+			setVisitorID(visitorId);
+		};
 
-    getUserID();
-  }, [setVisitorID]);
+		getUserID();
+	}, [setVisitorID]);
 
-  useEffect(() => {
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-      if (import.meta.env.VITE_API_REMOTE_URL) {
-        setApiHost(import.meta.env.VITE_API_REMOTE_URL);
-      } else {
-        setApiHost('http://localhost:8000/');
-      }
-    } else {
-      setApiHost('https://sws2apps.herokuapp.com/');
-    }
-  }, [setApiHost]);
+	useEffect(() => {
+		if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+			if (import.meta.env.VITE_API_REMOTE_URL) {
+				setApiHost(import.meta.env.VITE_API_REMOTE_URL);
+			} else {
+				setApiHost('http://localhost:8000/');
+			}
+		} else {
+			setApiHost('https://sws2apps.herokuapp.com/');
+		}
+	}, [setApiHost]);
 
-  useEffect(() => {
-    if (!indexedDB) {
-      if (!('serviceWorker' in navigator)) {
-        return (
-          <div className='browser-not-supported'>
-            You seem to use an unsupported browser to run this website. Make sure that you browser is up to date, or try to use
-            another browser.
-          </div>
-        );
-      }
-    }
-  }, []);
+	useEffect(() => {
+		if (!indexedDB) {
+			if (!('serviceWorker' in navigator)) {
+				return (
+					<div className='browser-not-supported'>
+						You seem to use an unsupported browser to run this website. Make sure that you browser is up to date, or try to use
+						another browser.
+					</div>
+				);
+			}
+		}
+	}, []);
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={activeTheme}>
-        <CssBaseline />
-        <InternetChecker />
-        {appSnackOpen && <NotificationWrapper />}
+	return (
+		<QueryClientProvider client={queryClient}>
+			<ThemeProvider theme={activeTheme}>
+				<CssBaseline />
+				<InternetChecker />
+				{appSnackOpen && <NotificationWrapper />}
 
-        <RouterProvider router={router} />
-      </ThemeProvider>
-    </QueryClientProvider>
-  );
+				<RouterProvider router={router} />
+			</ThemeProvider>
+		</QueryClientProvider>
+	);
 };
 
 export default App;
