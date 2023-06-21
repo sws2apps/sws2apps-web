5b0df34c43dda7d2f2e614f116b7eaf7e2af7dc7
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-06-21 15:47:40 +0300

    fix(congregations): rerender public talk import component

--- src/pages/PublicTalks.jsx
+++ src/pages/PublicTalks.jsx
@@ -1,4 +1,4 @@
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState } from 'recoil';
 import { useQuery } from '@tanstack/react-query';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
@@ -10,39 +10,40 @@ import { useEffect } from 'react';
 import { apiFetchPublicTalks } from '../api/congregation';
 
 const PublicTalks = () => {
-  const { isLoading, data } = useQuery({
-    queryKey: ['public_talks'],
-    queryFn: apiFetchPublicTalks,
-    staleTime: 60000,
-  });
+	const { isLoading, data } = useQuery({
+		queryKey: ['public_talks'],
+		queryFn: apiFetchPublicTalks,
+		staleTime: 60000,
+	});
 
-  const [publicTalks, setPublicTalks] = useRecoilState(publicTalksListState);
+	const [publicTalks, setPublicTalks] = useRecoilState(publicTalksListState);
+	const [openImport, setOpenImport] = useRecoilState(publicTalkImportOpenState);
 
-  const setOpenImport = useSetRecoilState(publicTalkImportOpenState);
+	const handleClickOpen = () => {
+		setOpenImport(true);
+	};
 
-  const handleClickOpen = () => {
-    setOpenImport(true);
-  };
+	useEffect(() => {
+		if (!isLoading) setPublicTalks(data);
+	}, [isLoading, data, setPublicTalks]);
 
-  useEffect(() => {
-    if (!isLoading) setPublicTalks(data);
-  }, [isLoading, data, setPublicTalks]);
+	return (
+		<Box>
+			<Typography variant='h6'>PUBLIC TALKS</Typography>
+			<Button variant='outlined' startIcon={<ImportExportIcon />} sx={{ marginTop: '20px' }} onClick={handleClickOpen}>
+				Import JSON
+			</Button>
 
-  return (
-    <Box>
-      <Typography variant="h6">PUBLIC TALKS</Typography>
-      <Button variant="outlined" startIcon={<ImportExportIcon />} sx={{ marginTop: '20px' }} onClick={handleClickOpen}>
-        Import JSON
-      </Button>
-      <PublicTalkImport />
-      <Box sx={{ margin: '20px 0', maxWidth: '900px', display: 'flex', flexDirection: 'column', gap: '25px' }}>
-        {publicTalks.map((talk) => (
-          <PublicTalkContainer key={talk.talk_number} talk_number={talk.talk_number} />
-        ))}
-        <PublicTalkContainer isNew={true} />
-      </Box>
-    </Box>
-  );
+			{openImport && <PublicTalkImport />}
+
+			<Box sx={{ margin: '20px 0', maxWidth: '900px', display: 'flex', flexDirection: 'column', gap: '25px' }}>
+				{publicTalks.map((talk) => (
+					<PublicTalkContainer key={talk.talk_number} talk_number={talk.talk_number} />
+				))}
+				<PublicTalkContainer isNew={true} />
+			</Box>
+		</Box>
+	);
 };
 
 export default PublicTalks;
