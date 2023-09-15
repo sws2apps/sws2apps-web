593ec9e9932190eb16c5222dbb6c5815d47381b8
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-09-15 13:20:53 +0300

    feat(web): update languages list

--- src/constant/langList.js
+++ src/constant/langList.js
@@ -1,5 +1,8 @@
 export const LANGUAGE_LIST = [
-  { code: 'e', locale: 'en', name: 'English', isSource: true },
-  { code: 'mg', locale: 'mg-MG', name: 'Malagasy' },
-  { code: 't', locale: 'pt-BR', name: 'Português (Brasil)' },
+	{ code: 'x', locale: 'de-DE', name: 'Deutsch' },
+	{ code: 'e', locale: 'en', name: 'English', isSource: true },
+	{ code: 'mg', locale: 'mg-MG', name: 'Malagasy' },
+	{ code: 't', locale: 'pt-BR', name: 'Português (Brasil)' },
+	{ code: 'ttm', locale: 'mg-TTM', name: 'Tenin’ny Tanana Malagasy' },
+	{ code: 'k', locale: 'uk-UA', name: 'Українська' },
 ];
